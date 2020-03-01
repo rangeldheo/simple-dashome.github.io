6808c55967462d88e41f2bc42c877507ecbab26c
rangeldheo <rangeldheo@gmail.com>
2020-03-01 02:22:51 -0300

    Update Chart bar

--- public/js/progress-script.js
+++ public/js/progress-script.js
@@ -1,8 +1,17 @@
 $(document).ready(function () {
-    $.each($('.progress'), function (indexInArray, valueOfElement) {
-        value = $(this).data('progress');
+    $.each($('.progress'), function () {
+        let value = $(this).data('progress');
         $(this).animate({
-            'width' : value + '%'
-        },'slow');     
+            'width': value + '%'
+        }, 'slow');
+    });
+    $.each($('.item-bar'), function () {
+        let range = $('.verticalBars').data('range');
+        let multiple = $('.verticalBars').height();
+        let value = $(this).data('value');
+        let height = (value * 100) / range;       
+        $(this).animate({
+            'height': height + '%'
+        }, 'slow');
     });
 });

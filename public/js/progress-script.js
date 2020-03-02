356b086aada4c92cb151b29e16801f02dc9d368f
rangeldheo <rangeldheo@gmail.com>
2020-03-01 22:01:53 -0300

    Udapte

--- public/js/progress-script.js
+++ public/js/progress-script.js
@@ -1,15 +1,50 @@
+/**
+ |---------------------------------------------------------
+ | by Rangel Dheo 01/03/2020
+ | rangeldheo@gmail.com
+ |---------------------------------------------------------
+ */
 $(document).ready(function () {
     $.each($('.progress'), function () {
+
         let value = $(this).data('progress');
+        /**
+         |---------------------------------------------------------
+         | se o valor for menor que 50%, marca o 
+         | ancestral como bar-netagive para estilizar dinamicamente
+         |---------------------------------------------------------
+         */
+        if (value < 50) {
+            $(this).parents('.bar').addClass('bar-negative');
+        }
+        /**
+         |---------------------------------------------------------
+         | baseado no valor do atributo data-value, criamos a
+         | animação dinamica da regra css width do elemento
+         |---------------------------------------------------------
+         */
         $(this).animate({
             'width': value + '%'
         }, 'slow');
+
     });
     $.each($('.item-bar'), function () {
-        let range = $('.verticalBars').data('range');
-        let multiple = $('.verticalBars').height();
+        /**
+         |---------------------------------------------------------
+         | O range é o valor máximo encontrado
+         | Essa será a barra mais alta e as outras 
+         | terão tamanho relativo percentual a mais alta
+         |---------------------------------------------------------
+         */
+        let range = $('.verticalBars').data('range');   
         let value = $(this).data('value');
-        let height = (value * 100) / range;       
+        /**
+         |---------------------------------------------------------
+         | Regrinha de 3 pra encontrar a altura relativa das barras
+         |---------------------------------------------------------
+         */
+        let height = (value * 100) / range;
+
         $(this).animate({
             'height': height + '%'
         }, 'slow');
